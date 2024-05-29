13abe7fafc551b6fe9c771b6bf0c3697d2cc13fd
Waleed Tariq <Waleedtariq47@yahoo.com>
2024-05-29 15:20:41 +0300

    Create an append function that adds a node to end of the list

--- linkedList.js
+++ linkedList.js
@@ -11,15 +11,33 @@ export default function linkedList() {
 
   const tail = () => {
     let pointer = head();
+    // to keep pointing to the next pointer until its the last node
     while (pointer.next !== null) {
       pointer = pointer.next;
     }
     return pointer;
   };
 
+  const append = (value) => {
+    let pointer = head();
+    // to add the node to the head in case the list is empty
+    if (pointer === null) {
+      list.head = node(value);
+    } else {
+      // to keep pointing the next item until its the last item
+      while (pointer.next !== null) {
+        pointer = pointer.next;
+      }
+      if (pointer.next === null) {//!!i think this if isn't necessary ill get back to it later
+        pointer.next = node(value);
+      }
+    }
+  };
+
   return {
     head,
     tail,
+    append,
   };
 }
 
